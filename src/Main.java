b488bbd986e55053802e4606b16c0410fa27a8f2
VladimirKing <cheremuha1@gmail.com>
2021-01-10 22:13:59 +0200

    src/

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 0;
+        int z = 0;
+        String ss = "!";
+        while (x <= 120000) {
+            x++;
+            char a = (char) x;
+            System.out.print(a + " ");
+            String s = String.valueOf(a);
+            if (s.equals("K")) {
+                z = x;
+            }
+        }
+
+        System.out.println();
+        System.out.println(z + " = " + ss);
+
+    }
+}
