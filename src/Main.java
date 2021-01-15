9d766b3db7322311d2b833717fd0840726e033cc
VladimirKing <cheremuha1@gmail.com>
2021-01-15 02:29:18 +0200

    aa

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         ArrayList <Integer>list =new ArrayList();
         int x = 0;
         int z = 0;
-        String ss = "!";
+        String ss = "Ġ";
         while (x <= 120000) {
             x++;
             char a = (char) x;
@@ -21,6 +21,9 @@ for (int i:list){
 
         System.out.println(i + " = " + ss);}
         System.out.println(ss.codePointAt(0));// схоже що є два однаковіх занака і юнікоді
+int[]in={1,3};//продолжить эту тему
+      //  String as= 22,5;
+        System.out.println(ss.indexOf(288));
 
     }
 }
