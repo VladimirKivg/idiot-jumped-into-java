5fa27cf7e6d07e57ff647eac962163abc6741219
VladimirKing <cheremuha1@gmail.com>
2021-01-25 13:10:25 +0200

    aa

--- src/Ded.java
+++ src/Ded.java
@@ -6,4 +6,31 @@ String name;
         this.i = i;
         this.name = name;
     }
+
+    public Ded() {
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Ded{" +
+                "i=" + i +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
