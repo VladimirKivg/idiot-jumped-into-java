d4783ff90f59d2f6fbafaa1d75dc1fbdbc4107c1
VladimirKing <cheremuha1@gmail.com>
2021-05-10 14:57:55 +0300

    work

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList <Ded>list =new ArrayList();
+        ArrayList<Ded> list = new ArrayList();
         int x = 0;
         int z = 0;
         Ded ded = new Ded();
@@ -13,29 +13,29 @@ public class Main {
             System.out.print(a + " ");
             String s = String.valueOf(a);
             if (s.equals(ss)) {
-                System.out.println(x+" = "+a);
+                System.out.println(x + " = " + a);
                 ded.setI(x);
                 ded.setName(s);
-               list.add(ded);
+                list.add(ded);
             }
         }
-        System.out.println(list.size()+"__________________________________________");
-for (Ded ded1:list){
+        System.out.println(list.size() + "__________________________________________");
+        for (Ded ded1 : list) {
 
-        System.out.println(ded1.getI() + " = " + ded1.getName());// для меня пока загадка , почему выводиться только паследние цыфры
-}
+            System.out.println(ded1.getI() + " = " + ded1.getName());// для меня пока загадка , почему выводиться только паследние цыфры
+        }
 
-       System.out.println(ss.codePointAt(0)+"____________");//виводить позицыю в юныкоды схоже що є два однаковіх занака і юнікоді
-int[]in={1,3};//продолжить эту тему
-      //  String as= 22,5;
+        System.out.println(ss.codePointAt(0) + "____________");//виводить позицыю в юныкоды схоже що є два однаковіх занака і юнікоді
+        int[] in = {1, 3};//продолжить эту тему
+        //  String as= 22,5;
         System.out.println(ss.indexOf(288));
         System.out.println("___________________________________________");
-        Time time =new Time();
+        Time time = new Time();
         time.timeExperiment();
         System.out.println("____________________________________________");
 
         StringBuilder sb = new StringBuilder("sss");
-        sb.append("SSS").insert(1,"vov").insert(3,"ttt");
+        sb.append("SSS").insert(1, "vov").insert(3, "ttt");
         System.out.println(sb);
 
     }
