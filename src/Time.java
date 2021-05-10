58486219c999bd6de0d2955dd3072549c0a39451
VladimirKing <cheremuha1@gmail.com>
2021-05-10 14:58:04 +0300

    work

--- src/Time.java
+++ src/Time.java
@@ -2,11 +2,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Time {
-    public void timeExperiment(){
-        LocalDateTime myDate=  LocalDateTime.now();
-        System.out.println("дата э така: "+myDate);
+    public void timeExperiment() {
+        LocalDateTime myDate = LocalDateTime.now();
+        System.out.println("дата э така: " + myDate);
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss:ms:ns");
-        System.out.println("формат дата эт така: "+myDate.format(dateTimeFormatter));
+        System.out.println("формат дата эт така: " + myDate.format(dateTimeFormatter));
 
     }
 }
